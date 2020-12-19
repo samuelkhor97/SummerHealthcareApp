7b5b4d36f300e643580234020a472fa3bd59f7dd
Hun Zi-Song <hunzisong@gmail.com>
2020-12-19 22:16:44 +0800

    Added firebase dependencies and SDK to current project

--- summer_healthcare_app/lib/main.dart
+++ summer_healthcare_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:summer_healthcare_app/constants.dart';
 import 'package:summer_healthcare_app/landing/landing_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(App());
 }
 

9c801787c0c28975abf14b7cc032cfa0a831da30
Hun Zi-Song <hunzisong@gmail.com>
2021-01-15 16:55:06 +0800

    Added pop up for weight change (in progress)

--- summer_healthcare_app/lib/home/user/monitoring/weight_page.dart
+++ summer_healthcare_app/lib/home/user/monitoring/weight_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:summer_healthcare_app/constants.dart';
+import 'package:summer_healthcare_app/widgets/widgets.dart';
 
 class WeightPage extends StatefulWidget {
   @override
@@ -14,33 +15,91 @@ class _WeightPageState extends State<WeightPage> {
     super.initState();
   }
 
-  Widget addWeight(date, weight, lastWeight) {
+  Widget addWeight(String date, TextEditingController weight, String lastWeight) {
+    String w = weight.text;
     return ListTile(
       title: Text(
         date.toString(),
         style: TextStyle(fontSize: FontSizes.normalText),
       ),
       trailing: Text(
-        weight.toString() + ' kg',
+        w + ' kg',
         style: TextStyle(
             fontSize: FontSizes.biggerText,
-            color: weight > lastWeight
+            color: int.parse(weight.text) > int.parse(lastWeight)
                 ? Colours.red
-                : weight < lastWeight
+                : int.parse(weight.text) < int.parse(lastWeight)
                     ? Colours.green
                     : Colours.grey),
       ),
+      onTap: () async {
+        TextEditingController newWeight = await showWeightChangePopUp(currentDate: date, weight: weight);
+        print('weight: ${newWeight.text}');
+        setState(() {
+          w = newWeight.text;
+        });
+      },
     );
+  }
+
+  Future<TextEditingController> showWeightChangePopUp({String currentDate, TextEditingController weight}) async {
+    await showDialog<TextEditingController>(
+      context: context,
+      builder: (BuildContext alertContext) {
+        return AlertDialog(
+          title: Text('Change Weight'),
+          content: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  InputField(
+                    hintText: '69',
+                    controller: weight,
+                    labelText: 'Weight (kg)',
+                    keyboardType: TextInputType.number,
+                    // onChanged: (String text) {
+                    //   setState(() {
+                    //     checkAllInformationFilled(checkBox: userDetails.termsAndConditions);
+                    //   });
+                    // },
+                  ),
+                  Text(
+                    'Date: $currentDate',
+                    style: TextStyle(
+                      color: Colours.grey
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('UPDATE'),
+              onPressed: () {
+                Navigator.of(alertContext).pop();
+                print('weight1: ${weight.text}');
+                return weight;
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return weight;
   }
 
   @override
   Widget build(BuildContext context) {
     if (weightList == null) {
       weightList = [
-        addWeight('15/1/2021', 66, 67),
-        addWeight('14/1/2021', 67, 70),
-        addWeight('13/1/2021', 70, 69),
-        addWeight('12/1/2021', 69, 69)
+        addWeight('15/1/2021', TextEditingController(text: '66'), '67'),
+        addWeight('14/1/2021', TextEditingController(text: '67'), '70'),
+        addWeight('13/1/2021', TextEditingController(text: '70'), '69'),
+        addWeight('12/1/2021', TextEditingController(text: '69'), '69')
       ];
     }
 

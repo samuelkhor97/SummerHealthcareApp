4b0c2c47aaee699daeae13dbcf9bd2d9a257125b
Jackie <jackiepangjiachee98@gmail.com>
2021-01-21 16:02:18 +0800

    Created number of steps card with live data.

--- summer_healthcare_app/lib/home/user/monitoring/mi_band_page.dart
+++ summer_healthcare_app/lib/home/user/monitoring/mi_band_page.dart
@@ -5,8 +5,9 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:summer_healthcare_app/constants.dart';
-import 'package:summer_healthcare_app/widgets/widgets.dart';
 import "package:http/http.dart" as http;
+
+import '../../../constants.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: [
@@ -24,6 +25,7 @@ class MiBandPage extends StatefulWidget {
 }
 
 class _MiBandPageState extends State<MiBandPage> {
+  var todaySteps = 0;
   FirebaseAuth _auth = FirebaseAuth.instance;
   GoogleSignInAccount _currentUser;
 
@@ -36,11 +38,12 @@ class _MiBandPageState extends State<MiBandPage> {
       });
       if (_currentUser != null) {
         _getFitData();
+      } else {
+        _handleSignIn();
       }
     });
     _googleSignIn.signInSilently();
   }
-
 
   /// Heart rate body
   // {
@@ -53,55 +56,169 @@ class _MiBandPageState extends State<MiBandPage> {
   // "startTimeMillis": 1609430400000
   // }
 
-
   Future<void> _getFitData() async {
+    var now = DateTime.now();
+    // print(now.toUtc().millisecondsSinceEpoch);
+    // print(DateTime(now.year, now.month, now.day).toUtc().millisecondsSinceEpoch);
     var body = jsonEncode({
-      "aggregateBy": [{
-        "dataTypeName": "com.google.step_count.delta",
-        "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
-      }],
-      "bucketByTime": { "durationMillis": 86400000 },
-      "startTimeMillis": 1609430400000,
-      "endTimeMillis": 1610812800000
+      "aggregateBy": [
+        {
+          "dataTypeName": "com.google.step_count.delta",
+          "dataSourceId":
+              "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+        }
+      ],
+      "bucketByTime": {"durationMillis": 86400000},
+      "startTimeMillis": DateTime(now.year, now.month, now.day - 7).toUtc().millisecondsSinceEpoch,
+      "endTimeMillis": now.toUtc().millisecondsSinceEpoch
     });
 
-    GoogleSignInAuthentication googlesigninauthentication = await _currentUser.authentication;
-    var response = await http
-        .post('https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
-        headers: {'authorization': 'Bearer ' + googlesigninauthentication.accessToken},
-        body: body
-        );
+    GoogleSignInAuthentication googlesigninauthentication =
+        await _currentUser.authentication;
+    var response = await http.post(
+        'https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate',
+        headers: {
+          'authorization': 'Bearer ' + googlesigninauthentication.accessToken
+        },
+        body: body);
     var output = jsonDecode(response.body);
-    print(output["bucket"][5]);
+    // The integer after bucket is subject to change, to get today's step value, then it's 7
+    print(output["bucket"][7]["dataset"][0]["point"][0]["value"][0]["intVal"]);
+    setState(() {
+      todaySteps = output["bucket"][7]["dataset"][0]["point"][0]["value"][0]["intVal"];
+    });
   }
 
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colours.primaryColour,
-        appBar: AppBar(
           backgroundColor: Colours.primaryColour,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: Colors.black),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+          appBar: AppBar(
+            backgroundColor: Colours.primaryColour,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            title: Text(
+              'Mi Band',
+            ),
+            centerTitle: true,
+            elevation: Dimensions.d_3,
           ),
-          title: Text(
-            'Mi Band',
-          ),
-          centerTitle: true,
-          elevation: Dimensions.d_3,
-        ),
-        body: Column(
-          children: <Widget>[
-            ElevatedButton(
-              child: const Text('SIGN IN'),
-              onPressed: _handleSignIn,
-            )
-          ],
-        ),
-      ),
+          body: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(
+                    top: Dimensions.d_15,
+                    left: Dimensions.d_15,
+                    right: Dimensions.d_15),
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  elevation: 5,
+                  child: Padding(
+                    padding: EdgeInsets.all(Dimensions.d_15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          leading: Column(
+                            children: <Widget>[
+                              Text(
+                                'Steps Today:',
+                                style: TextStyle(
+                                  fontSize: FontSizes.biggerText
+                                ),
+                              ),
+                              Text(
+                                '${todaySteps}',
+                                style: TextStyle(
+                                  fontSize: FontSizes.biggerText,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'Distance: ${(todaySteps/1312.33595801).toStringAsFixed(2)} KM',
+                                style: TextStyle(
+                                    fontSize: FontSizes.smallText
+                                ),
+                              )
+                            ],
+                          ),
+                          trailing: Icon(
+                            Icons.directions_run,
+                            color: Colours.secondaryColour,
+                            size: Dimensions.d_50,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              // Padding(
+              //   padding: EdgeInsets.only(
+              //       top: Dimensions.d_15,
+              //       left: Dimensions.d_15,
+              //       right: Dimensions.d_15),
+              //   child: Card(
+              //     clipBehavior: Clip.antiAlias,
+              //     elevation: 5,
+              //     child: Padding(
+              //       padding: EdgeInsets.all(Dimensions.d_15),
+              //       child: Column(
+              //         crossAxisAlignment: CrossAxisAlignment.start,
+              //         children: [
+              //           ListTile(
+              //             leading: Column(
+              //               children: <Widget>[
+              //                 Text(
+              //                   'Last Night Sleep Hours:',
+              //                   style: TextStyle(
+              //                       fontSize: FontSizes.biggerText
+              //                   ),
+              //                 ),
+              //                 Text(
+              //                   '8h 32min',
+              //                   style: TextStyle(
+              //                       fontSize: FontSizes.biggerText,
+              //                       fontWeight: FontWeight.bold
+              //                   ),
+              //                 ),
+              //                 Text(
+              //                   'Deep sleep: 2h 20min',
+              //                   style: TextStyle(
+              //                       fontSize: FontSizes.smallText
+              //                   ),
+              //                 ),
+              //                 Text(
+              //                   'Light sleep: 6h 12min',
+              //                   style: TextStyle(
+              //                       fontSize: FontSizes.smallText
+              //                   ),
+              //                 ),
+              //                 Text(
+              //                   'Awake: 0min',
+              //                   style: TextStyle(
+              //                       fontSize: FontSizes.smallText
+              //                   ),
+              //                 ),
+              //               ],
+              //             ),
+              //             trailing: Icon(
+              //               Icons.king_bed_rounded,
+              //               color: Colours.secondaryColour,
+              //               size: Dimensions.d_50,
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //   ),
+              // )
+            ],
+          )),
     );
   }
 
@@ -116,3 +233,12 @@ class _MiBandPageState extends State<MiBandPage> {
   // signout function
   Future<void> _handleSignOut() => _googleSignIn.disconnect();
 }
+
+// Column(
+// children: <Widget>[
+// ElevatedButton(
+// child: const Text('SIGN IN'),
+// onPressed: _handleSignIn,
+// )
+// ],
+// ),

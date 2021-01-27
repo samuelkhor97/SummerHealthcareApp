f222491feed1d7d13974ea28bd6abe9ac28f6011
Bryan Lee <hlee0015@student.monash.edu>
2021-01-27 10:58:09 +0800

    added api to add food item to a food diary card

--- foodDiary/server.js
+++ foodDiary/server.js
@@ -46,14 +46,9 @@ router.get('/all-cards', async (req, res, next) => {
     const uid = res.locals.id;
     const date = req.query.date;
 
-    let date_obj = new Date(date);
+    const date_obj = new Date(date);
 
     try{
-        // models.Food_Bridge.create({
-        //     card_id: body.card_id,
-        //     food_id: body.food_id
-        // });
-        var card_holder = [];
         const all_cards = await models.Food_Diary_Card.findAll({
             where: {
                 uid: uid,
@@ -63,64 +58,53 @@ router.get('/all-cards', async (req, res, next) => {
                 {
                     model: models.Food_Data,
                     attributes: ["food_id", "food_name", "calories"],
-                    // all: true,
                     through: {
                         attributes: [],
                     },
                 }
             ],
-            raw: true,
-            // group: "card_id",
             order: ["card_id"],
         });
-        
-        
-        console.log(all_cards.length);
+
         return res.status(200).json(all_cards);
     } catch (error) {
-        return res.status(403).send(error)
+        return res.status(403).send(error);
     }
 });
 
 
-
-/**1
- * GET api: Get all on-demand request (pending only) (SLI side)
- * url: domain/on-demand/all
+/**
+ * POST api: Add a food item to a card
+ * url: domain/food-diary/add-food
  */
-router.get('/all_request', async (req, res, next) => {
-    const sli_id = res.locals.id;
+router.post('/add-food', async (req, res, next) => {
+    const uid = res.locals.id;
+    const body = req.body;
+    const food_id = body.food_id;
+    const card_name = body.card_name;
+    const date = body.date;
+    const date_obj = new Date(date);
     
-    try {
-      
-      const result = await models.Booking.findAll({
-        where: {
-          sli_id: sli_id,
-          status: 'pending'
-        },
-        include: [
-          { model: models.User, attributes: [] },
-        ],
-        raw: true,
-        attributes:[
-          "booking_id",
-          "uid",
-          "sli_id",
-          "date",
-          "time",
-          "notes",
-          "status",
-          "hospital_name",
-          [models.Sequelize.col('User.name'), 'user_name'],
-          [models.Sequelize.col('User.gender'), 'user_gender'],
-          [models.Sequelize.col('User.age'), 'user_age'],
-          [models.Sequelize.col('User.profile_pic'), 'user_profile_pic']
-        ]
-      });
-  
-      return res.status(200).json(result);
+    try{
+        const food_card = await models.Food_Diary_Card.findOne({
+            where: {
+                uid: uid,
+                date: date_obj,
+                card_name: card_name
+            }
+        });
+
+        const card_id = food_card.card_id
+        
+        models.Food_Bridge.create({
+            card_id: card_id,
+            food_id: food_id
+        });
+        
+        return res.status(200).send('Added food item successfully');
     } catch (error) {
-      return res.status(403).send(error)
+        return res.status(403).send(error);
     }
-  });
+});
+
 module.exports = router;

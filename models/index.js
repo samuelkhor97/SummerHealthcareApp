9a6a78d6b2a4e4252dc3ec7ac05dea0331898a4f
Bryan Lee <hlee0015@student.monash.edu>
2021-01-28 11:21:16 +0800

    test

--- models/index.js
+++ models/index.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: 'postgres',
-    },
-);
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASS,
+//     {
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT,
+//         dialect: 'postgres',
+//     },
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const models = {
     User: require('./user')(sequelize, Sequelize),

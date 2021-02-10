60319213ff9e16f09b1dd29e0ac2c9117b6a8b9b
Ibrahim Mohamed Ragheb Ragheb Shaarawy <isha0004@student.monash.edu>
2021-02-10 20:58:31 +0200

    Added readings page and WebView Package

--- summer_healthcare_app/lib/home/user/readings/readings_page.dart
+++ summer_healthcare_app/lib/home/user/readings/readings_page.dart
@@ -1,13 +1,205 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ReadingsPage extends StatefulWidget {
   @override
   _ReadingsPageState createState() => _ReadingsPageState();
 }
 
-class _ReadingsPageState extends State<ReadingsPage> {
+class _ArticleDescription extends StatelessWidget {
+  _ArticleDescription({
+    Key key,
+    this.title,
+    this.subtitle,
+    this.author,
+    this.publishDate,
+    this.readDuration,
+  }) : super(key: key);
+
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                '$title',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(bottom: 2.0)),
+              Text(
+                '$subtitle',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text(
+                '$author',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '$publishDate - $readDuration',
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ArticleItem extends StatelessWidget {
+  ArticleItem({
+    Key key,
+    this.thumbnail,
+    this.title,
+    this.subtitle,
+    this.author,
+    this.publishDate,
+    this.readDuration,
+  }) : super(key: key);
+
+  final Widget thumbnail;
+  final String title;
+  final String subtitle;
+  final String author;
+  final String publishDate;
+  final String readDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: SizedBox(
+        height: 100,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 1.0,
+              child: thumbnail,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 2.0, 0.0),
+                child: _ArticleDescription(
+                  title: title,
+                  subtitle: subtitle,
+                  author: author,
+                  publishDate: publishDate,
+                  readDuration: readDuration,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ArticleView extends StatelessWidget {
+  final String title;
+  final String selectedUrl;
+
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  ArticleView({
+    @required this.title,
+    @required this.selectedUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: WebView(
+          initialUrl: selectedUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+        ));
+  }
+}
+
+class _ReadingsPageState extends State<ReadingsPage> {
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(10.0),
+      children: <Widget>[
+        FlatButton(
+          child: ArticleItem(
+            thumbnail: Container(
+              decoration: const BoxDecoration(color: Colors.pink),
+            ),
+            title: 'Flutter 1.0 Launch',
+            subtitle: 'Flutter continues to improve and expand its horizons.'
+                'This text should max out at two lines and clip',
+            author: 'Dash',
+            publishDate: 'Dec 28',
+            readDuration: '5 mins',
+          ),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (BuildContext context) => ArticleView(
+                      title: "Alligator.io",
+                      selectedUrl: "https://youtube.com",
+                    )));
+          },
+        ),
+        FlatButton(
+          child: ArticleItem(
+            thumbnail: Container(
+              decoration: const BoxDecoration(color: Colors.blue),
+            ),
+            title: 'Flutter 1.2 Release - Continual updates to the framework',
+            subtitle: 'Flutter once again improves and makes updates.',
+            author: 'Flutter',
+            publishDate: 'Feb 26',
+            readDuration: '12 mins',
+          ),
+          onPressed: () => {print('Sha3ro 2')},
+        ),
+      ],
+    );
   }
 }
